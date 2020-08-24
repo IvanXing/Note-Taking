49151feafb50ae8610d830cbb185bdc7e398eb3f
xingyifan <xingyifan10@qq.com>
2020-08-24 23:36:14 +0800

    fix note

--- 02.promise/history4/1.promise.js
+++ 02.promise/history4/1.promise.js
@@ -15,7 +15,7 @@ promise2.then((data) => {
 
 
 /*
-** promise2 返回了一个 promise2 没有调用成功或者失败
+** promise2 返回了一个 promise2 没有调用成功或者失败，会自己等自己完成
 ** 规范规定报错 类型错误 TypeError: Chaining cycle detected for promise #<Promise>
 */
 
