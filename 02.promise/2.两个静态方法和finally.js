f1d3380af5e6f603045b072b07dfe1410967337d
xingyifan <xingyifan10@qq.com>
2020-08-25 00:47:54 +0800

    finally实现

--- 02.promise/2.两个静态方法和finally.js
+++ 02.promise/2.两个静态方法和finally.js
@@ -16,15 +16,24 @@ Promise.resolve(new Promise((resolve,reject)=>{
     console.log(data)
 })
 
+/*
+** finally,不管前面是成功还是失败，都执行
+** finally也可以返回promise
+*/
+
 // finally表示不是最终的意思 而是无论如何都会执行的意思
 // 如果返回一个promise 会等待这个promise 也执行完毕，（如果是失败的promise 会用他的失败原因传给下一个人）
+/*
+** 实现
+*/
 Promise.prototype.finally = function (callback) {
     return this.then((value)=>{
-        return Promise.resolve(callback()).then(()=>value)
+        return Promise.resolve(callback()).then(()=>value)  // then里返回一个promise，就会等待直到普通值
     },(reason)=>{
         return Promise.resolve(callback()).then(()=>{throw reason})
     })  
 }
+// 用法
 Promise.resolve(456).finally(()=>{
     return new Promise((resolve,reject)=>{
         setTimeout(() => {
