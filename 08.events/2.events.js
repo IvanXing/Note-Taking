0eb8a54e2e59186519b07f6789fbacb97c318913
Ivan Xing <xingyifan10@qq.com>
2020-08-27 13:58:00 +0800

    event模块实现

--- 08.events/2.events.js
+++ 08.events/2.events.js
@@ -1,11 +1,16 @@
 const EventEmitter = require('./events');
 const util = require('util');
+
 function Girl() {}
 util.inherits(Girl, EventEmitter);
-let girl = new Girl();
-// 发布订阅 “发布”  “订阅” [fn,fn,fn...]
 
-girl.on('newListener', (type) => { // newListener 固定写法
+let girl = new Girl();
+
+// 发布订阅 “发布”，“订阅” 把方法存数组[fn,fn,fn...]
+
+// 订阅
+// newListener 固定写法
+girl.on('newListener', (type) => { 
     console.log(type)
     if (type === '女生失恋') {
         process.nextTick(() => { // 在当前同步代码执行完毕后触发事件
@@ -17,16 +22,16 @@ girl.on('newListener', (type) => { // newListener 固定写法
 const cry = (w) => console.log('哭', w);
 const eat = (w) => console.log('吃', w);
 girl.once('女生失恋', cry);
-girl.once('女生失恋', eat); // {女生失恋:[once.l=cry,once.l=eat]}
+girl.once('女生失恋', eat); // {女生失恋:[once.l=cry,once.l=eat]} 内部存的是once
 
 
-// girl.off('女生失恋',cry);
+girl.off('女生失恋',cry); // 关闭方法，找到数组删掉
 
 
-// vue $on $emit
+// vue 中$on $emit 就是参考这种写法
 
-// girl.emit('女生失恋', '小美')
-// girl.emit('女生失恋', '小美')
+girl.emit('女生失恋', '小美')
+girl.emit('女生失恋', '小美')
 
 // on emit off once newListener 发布订阅中应该有的方法 （好处：可以实现解耦合）
 
--- 08.events/3.events实现.js
+++ 08.events/3.events实现.js
@@ -1,20 +1,28 @@
+/*
+** event实现原理
+*/
+
 function EventEmitter (){
     this._events = {}; // 默认给EventEmitter准备的
 }
+ 
 EventEmitter.prototype.on = function (eventName,callback) {
+    // this是实例，判断实例上有没有这个属性
     if(!this._events) this._events = {};
     // 如果不是newListener 那就需要触发newListener的回调
     if(eventName !== 'newListener'){
         this.emit('newListener',eventName)
     }
-
+    // 先去找eventName，没有的话放一个数组，放入一个callback
     (this._events[eventName] || (this._events[eventName] = [])).push(callback)
 }
+
 EventEmitter.prototype.emit = function (eventName,...args) {
     if(this._events && this._events[eventName]){
         this._events[eventName].forEach(event =>event(...args));
     }
 }
+
 EventEmitter.prototype.off = function (eventName,callback) {
     // 先找到对应的数组
     if(this._events && this._events[eventName]){
@@ -23,6 +31,8 @@ EventEmitter.prototype.off = function (eventName,callback) {
         this._events[eventName] = this._events[eventName].filter(cb=>(cb != callback)&&(cb.l !== callback))
     }
 }   
+
+// 触发一次就移除，先绑定执行完才能移除，切片
 EventEmitter.prototype.once = function (eventName,callback) {
     const once =(...args)=> {
         callback(...args);
@@ -31,6 +41,12 @@ EventEmitter.prototype.once = function (eventName,callback) {
     once.l = callback; // 给once增加callback的标识
     this.on(eventName,once); // 先绑定一个一次性事件，稍后触发时，再将事件清空
 }
-// 观察者模式 会有两个类，观察者会被存到被观察者中，如果被观察者状态变化，会主动通知观察者，调用观察者的更新方法
+
+
+// 观察者模式，会有两个类，观察者会被存到被观察者中，如果被观察者状态变化，会主动通知观察者，调用观察者的更新方法
 // 观察者模式和发布订阅的场景 区分
+
+/*
+** 倒出
+*/
 module.exports = EventEmitter;
